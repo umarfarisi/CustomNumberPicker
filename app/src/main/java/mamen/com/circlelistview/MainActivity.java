20ff64d88d0f24ced84409c777e88d7a0a2fac5f
umarfarisi <pharese@gmail.com>
2017-03-25 20:28:28 +0700

    fix bug

--- app/src/main/java/mamen/com/circlelistview/MainActivity.java
+++ app/src/main/java/mamen/com/circlelistview/MainActivity.java
@@ -2,13 +2,12 @@ package mamen.com.circlelistview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
-import android.widget.NumberPicker;
-import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,69 +20,117 @@ public class MainActivity extends AppCompatActivity {
 
         layout = (LinearLayout) findViewById(R.id.layout);
 
-        layout.setOnTouchListener(new View.OnTouchListener() {
+        layout.setOnTouchListener(new JJBTNumberPicker(0,24));
 
-            final float DEFAULT_PREV_Y = -240;
+    }
 
-            float eventY = 0;
-            float prevYCounter = -240;
-            float currentYCounter = 0;
-            float nextYCounter = 240;
-            View prev = null, current = null, next = null;
+    public static class JJBTNumberPicker implements View.OnTouchListener{
 
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
+        public static final int PREV_INDEX = 0;
+        public static final int CURRENT_INDEX = 1;
+        public static final int NEXT_INDEX = 2;
 
-                if(event.getAction() == MotionEvent.ACTION_MOVE || event.getY() == MotionEvent.ACTION_BUTTON_RELEASE) {
+        public static final float DIFFERENT = 10;
+        public static final float DEFAULT_CURRENT_Y = 0;
+        public static final float DEFAULT_PREV_Y = -240;
+        public static final float DEFAULT_NEXT_Y = 240;
 
-                    if (prev == null || current == null || next == null){
-                        prev = layout.getChildAt(0);
-                        current = layout.getChildAt(1);
-                        next = layout.getChildAt(2);
-                    }
+        private float eventY;
 
-                    float different = eventY - event.getY();
+        private View prev;
+        private View current;
+        private View next;
 
-                    if(different > 0){ // keatas
+        private List<Integer> data;
 
-                        prev.setY(prev.getY() - 10);
-                        current.setY(current.getY() - 10);
-                        next.setY(next.getY() - 10);
+        public JJBTNumberPicker(int from, int until) {
+            eventY = 0;
+            prev = null;
+            current = null;
+            next = null;
+            data = new ArrayList<>();
+            for(int datum = from ; datum <= until ; datum++)data.add(datum);
+        }
 
-                        if(next.getY() == 0){
-                            prev.setY(240);
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
 
-                            View temp = prev;
-                            current = next;
-                            prev = current;
-                            next = temp;
-                        }
+            if(event.getAction() == MotionEvent.ACTION_MOVE || event.getY() == MotionEvent.ACTION_BUTTON_RELEASE) {
 
-                    }else { //kebawah
+                LinearLayout layout = (LinearLayout) v;
 
-                        next.setY(next.getY() + 10);
-                        current.setY(current.getY() + 10);
-                        prev.setY(prev.getY() + 10);
-
-                        if(prev.getY() == 0){
-                            next.setY(-240);
-
-                            View temp = current;
-                            current = prev;
-                            prev = next;
-                            next = temp;
-                        }
-
-                    }
-
-                    eventY = event.getY();
-
+                if (prev == null || current == null || next == null){
+                    prev = layout.getChildAt(PREV_INDEX);
+                    current = layout.getChildAt(CURRENT_INDEX);
+                    next = layout.getChildAt(NEXT_INDEX);
                 }
 
-                return true;
-            }
-        });
+                boolean isScrollUp = eventY - event.getY() > 0;
 
+                if(isScrollUp){ // keatas
+                    prev.setY(prev.getY() - DIFFERENT);
+                    current.setY(current.getY() - DIFFERENT);
+                    next.setY(next.getY() - DIFFERENT);
+
+                    if(next.getY() == DEFAULT_CURRENT_Y){
+                        prev.setY(DEFAULT_NEXT_Y);
+
+                        View temp = prev;
+                        prev = current;
+                        current = next;
+                        next = temp;
+                    }
+                }else { //kebawah
+                    next.setY(next.getY() + DIFFERENT);
+                    current.setY(current.getY() + DIFFERENT);
+                    prev.setY(prev.getY() + DIFFERENT);
+
+                    if(prev.getY() == DEFAULT_CURRENT_Y){
+                        next.setY(DEFAULT_PREV_Y);
+
+                        View temp = current;
+                        current = prev;
+                        prev = next;
+                        next = temp;
+                    }
+                }
+
+                eventY = event.getY();
+
+            }
+
+            return true;
+        }
+
+        private void setScrollDown() {
+            next.setY(next.getY() + DIFFERENT);
+            current.setY(current.getY() + DIFFERENT);
+            prev.setY(prev.getY() + DIFFERENT);
+
+            if(prev.getY() == DEFAULT_CURRENT_Y){
+                next.setY(DEFAULT_PREV_Y);
+
+                View temp = current;
+                current = prev;
+                prev = next;
+                next = temp;
+            }
+        }
+
+        private void setScrollUp() {
+            prev.setY(prev.getY() - DIFFERENT);
+            current.setY(current.getY() - DIFFERENT);
+            next.setY(next.getY() - DIFFERENT);
+
+            if(next.getY() == DEFAULT_CURRENT_Y){
+                prev.setY(DEFAULT_NEXT_Y);
+
+                View temp = prev;
+                prev = current;
+                current = next;
+                next = temp;
+            }
+        }
     }
 
 }
