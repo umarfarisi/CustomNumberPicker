1aedba0077ebc5ac3506622d07ece3ee523187cd
umarfarisi <pharese@gmail.com>
2017-03-25 22:41:24 +0700

    fix increment and decrement

---
+++ app/src/main/java/mamen/com/circlelistview/JJBTNumberPicker.java
@@ -0,0 +1,190 @@
+package mamen.com.circlelistview;
+
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class JJBTNumberPicker extends LinearLayout implements View.OnTouchListener{
+
+    public static final int PREV_INDEX = 0;
+    public static final int CURRENT_INDEX = 1;
+    public static final int NEXT_INDEX = 2;
+
+    public static final float DIFFERENT = 10;
+    public static final float DEFAULT_CURRENT_Y = 0;
+    public static final float DEFAULT_PREV_Y = -240;
+    public static final float DEFAULT_NEXT_Y = 240;
+
+    private float oldY;
+
+    private TextView prev;
+    private TextView current;
+    private TextView next;
+
+    private List<String> data;
+
+    private int indexOfCurrent;
+
+    public JJBTNumberPicker(Context context) {
+        super(context);
+        setUpDefaultValue();
+    }
+
+    public JJBTNumberPicker(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setUpDefaultValue();
+    }
+
+    public JJBTNumberPicker(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        setUpDefaultValue();
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public JJBTNumberPicker(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        setUpDefaultValue();
+    }
+
+
+    private void setUpDefaultValue() {
+        oldY = 0;
+
+        indexOfCurrent = 0;
+        data = new ArrayList<>();
+
+        prev = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.item,this,false);
+        addView(prev);
+        current = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.item,this,false);
+        addView(current);
+        next = (TextView) LayoutInflater.from(getContext()).inflate(R.layout.item,this,false);
+        addView(next);
+    }
+
+    public void setValue(int from, int until, int startFromIndex){
+        indexOfCurrent = startFromIndex;
+
+        for(int datum = from ; datum <= until ; datum++)data.add(String.valueOf(datum));
+
+        if(indexOfCurrent==0)prev.setText(data.get(data.size()-1));
+        else prev.setText(data.get(indexOfCurrent-1));
+
+        current.setText(data.get(indexOfCurrent));
+
+        if(indexOfCurrent==data.size()-1)next.setText(data.get(0));
+        else next.setText(data.get(indexOfCurrent+1));
+
+        setOnTouchListener(this);
+
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        if(event.getAction() == MotionEvent.ACTION_MOVE) {
+
+            boolean isScrollUp = oldY - event.getY() > 0.1;
+
+            if(isScrollUp){ // keatas itemnya
+                prev.setY(prev.getY() - DIFFERENT);
+                current.setY(current.getY() - DIFFERENT);
+                next.setY(next.getY() - DIFFERENT);
+
+                if(next.getY() <= DEFAULT_CURRENT_Y){
+
+                    prev.setY(DEFAULT_NEXT_Y);
+
+                    TextView temp = prev;
+                    prev = current;
+                    current = next;
+                    next = temp;
+
+                    indexOfCurrent++;
+
+                    if(indexOfCurrent == data.size()){
+                        indexOfCurrent = 0;
+                    }
+
+                    if(indexOfCurrent+1 == data.size()){
+                        next.setText(data.get(0));
+                    }else{
+                        next.setText(data.get(indexOfCurrent+1));
+                    }
+
+                }
+            }else { //kebawah
+                next.setY(next.getY() + DIFFERENT);
+                current.setY(current.getY() + DIFFERENT);
+                prev.setY(prev.getY() + DIFFERENT);
+
+                if(prev.getY() >= DEFAULT_CURRENT_Y){
+                    next.setY(DEFAULT_PREV_Y);
+
+                    TextView temp = current;
+                    current = prev;
+                    prev = next;
+                    next = temp;
+
+                    indexOfCurrent--;
+
+                    if(indexOfCurrent == -1){
+                        indexOfCurrent = data.size()-1;
+                    }
+
+                    if(indexOfCurrent-1 == -1){
+                        prev.setText(data.get(data.size()-1));
+                    }else {
+                        prev.setText(data.get(indexOfCurrent-1));
+                    }
+
+                }
+            }
+
+            oldY = event.getY();
+
+        }
+
+        return true;
+    }
+
+    private void setScrollDown() {
+        next.setY(next.getY() + DIFFERENT);
+        current.setY(current.getY() + DIFFERENT);
+        prev.setY(prev.getY() + DIFFERENT);
+
+        if(prev.getY() == DEFAULT_CURRENT_Y){
+            next.setY(DEFAULT_PREV_Y);
+
+            TextView temp = current;
+            current = prev;
+            prev = next;
+            next = temp;
+        }
+    }
+
+    private void setScrollUp() {
+        prev.setY(prev.getY() - DIFFERENT);
+        current.setY(current.getY() - DIFFERENT);
+        next.setY(next.getY() - DIFFERENT);
+
+        if(next.getY() == DEFAULT_CURRENT_Y){
+            prev.setY(DEFAULT_NEXT_Y);
+
+            TextView temp = prev;
+            prev = current;
+            current = next;
+            next = temp;
+        }
+    }
+}
--- app/src/main/java/mamen/com/circlelistview/MainActivity.java
+++ app/src/main/java/mamen/com/circlelistview/MainActivity.java
@@ -2,166 +2,20 @@ package mamen.com.circlelistview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
-import android.view.MotionEvent;
-import android.view.View;
-import android.widget.LinearLayout;
-import android.widget.TextView;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    LinearLayout layout;
+    JJBTNumberPicker numberPicker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        layout = (LinearLayout) findViewById(R.id.layout);
-
-        layout.setOnTouchListener(new JJBTNumberPicker(0,5,0));
+        numberPicker = (JJBTNumberPicker) findViewById(R.id.numberPicker);
+        numberPicker.setValue(0,3,0);
 
     }
 
-    public static class JJBTNumberPicker implements View.OnTouchListener{
-
-        public static final int PREV_INDEX = 0;
-        public static final int CURRENT_INDEX = 1;
-        public static final int NEXT_INDEX = 2;
-
-        public static final float DIFFERENT = 10;
-        public static final float DEFAULT_CURRENT_Y = 0;
-        public static final float DEFAULT_PREV_Y = -240;
-        public static final float DEFAULT_NEXT_Y = 240;
-
-        private float eventY;
-
-        private TextView prev;
-        private TextView current;
-        private TextView next;
-
-        private List<String> data;
-
-        private int index;
-
-        public JJBTNumberPicker(int from, int until, int startFromIndex) {
-            eventY = 0;
-
-            index = startFromIndex;
-
-            data = new ArrayList<>();
-            for(int datum = from ; datum <= until ; datum++)data.add(String.valueOf(datum));
-
-        }
-
-        @Override
-        public boolean onTouch(View v, MotionEvent event) {
-
-            if(event.getAction() == MotionEvent.ACTION_MOVE || event.getY() == MotionEvent.ACTION_BUTTON_RELEASE) {
-
-                if(prev == null || current == null || next == null){
-
-                    LinearLayout layout = (LinearLayout) v;
-
-                    prev = (TextView) layout.getChildAt(PREV_INDEX);
-                    current = (TextView) layout.getChildAt(CURRENT_INDEX);
-                    next = (TextView) layout.getChildAt(NEXT_INDEX);
-
-                    if(index==0)prev.setText(data.get(data.size()-1));
-                    else prev.setText(data.get(index-1));
-                    current.setText(data.get(index));
-                    if(index==data.size()-1)next.setText(data.get(0));
-                    else next.setText(data.get(index+1));
-                }
-
-                boolean isScrollUp = eventY - event.getY() > 0;
-
-                if(isScrollUp){ // keatas
-                    prev.setY(prev.getY() - DIFFERENT);
-                    current.setY(current.getY() - DIFFERENT);
-                    next.setY(next.getY() - DIFFERENT);
-
-                    if(next.getY() == DEFAULT_CURRENT_Y){
-
-                        prev.setY(DEFAULT_NEXT_Y);
-
-                        prev.setText(data.get(index));
-
-                        TextView temp = prev;
-                        prev = current;
-                        current = next;
-                        next = temp;
-
-                        index++;
-
-                        if(index == data.size()){
-                            index = 0;
-                        }
-
-                    }
-                }else { //kebawah
-                    next.setY(next.getY() + DIFFERENT);
-                    current.setY(current.getY() + DIFFERENT);
-                    prev.setY(prev.getY() + DIFFERENT);
-
-                    if(prev.getY() == DEFAULT_CURRENT_Y){
-                        next.setY(DEFAULT_PREV_Y);
-
-                        next.setText(data.get(index));
-
-                        TextView temp = current;
-                        current = prev;
-                        prev = next;
-                        next = temp;
-
-                        index--;
-
-                        if(index == -1){
-                            index = data.size()-1;
-                        }
-
-                    }
-                }
-
-                eventY = event.getY();
-
-            }
-
-            return true;
-        }
-
-        private void setScrollDown() {
-            next.setY(next.getY() + DIFFERENT);
-            current.setY(current.getY() + DIFFERENT);
-            prev.setY(prev.getY() + DIFFERENT);
-
-            if(prev.getY() == DEFAULT_CURRENT_Y){
-                next.setY(DEFAULT_PREV_Y);
-
-                TextView temp = current;
-                current = prev;
-                prev = next;
-                next = temp;
-            }
-        }
-
-        private void setScrollUp() {
-            prev.setY(prev.getY() - DIFFERENT);
-            current.setY(current.getY() - DIFFERENT);
-            next.setY(next.getY() - DIFFERENT);
-
-            if(next.getY() == DEFAULT_CURRENT_Y){
-                prev.setY(DEFAULT_NEXT_Y);
-
-                TextView temp = prev;
-                prev = current;
-                current = next;
-                next = temp;
-            }
-        }
-    }
 
 }
